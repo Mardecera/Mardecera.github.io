0f2fc86ad84f38bf00f345d2ae8a3b6013307ae4
mardecera <mardecera.personal@gmail.com>
2021-11-02 17:49:43 -0500

    [Issue #1] ● Implemented the function to return to the top of the page

---
+++ scripts/functions.js
@@ -0,0 +1,41 @@
+const $ = (query) => document.querySelector(query)
+$('#switch__theme').onclick = () => {
+    $('#switch__theme').classList.toggle('active')
+}
+$('.bar__close').onclick = () => {
+    $('.nav__sections').classList.toggle('hidden')
+}
+
+$('.nav__bar').onclick = () => {
+    $('.nav__sections').classList.toggle('hidden')
+}
+
+const $$ = (item) => document.querySelectorAll(item)
+$('.skill__frontend .skill__tab').onclick = () => {
+    $('.skill__frontend .skill__details').classList.toggle('hidden')
+}
+$('.skill__backend .skill__tab').onclick = () => {
+    $('.skill__backend .skill__details').classList.toggle('hidden')
+}
+$('.skill__desing .skill__tab').onclick = () => {
+    $('.skill__desing .skill__details').classList.toggle('hidden')
+}
+
+$('#web__top').onclick = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+}
+
+function openNewTab(url) {
+    window.open(url, '_blank', 'noopener, noreferrer')
+}
+
+function loadHiperLinks() {
+    const message = `Hola Jonathan, que tal?\nTienes tiempo para charlar sobre una propuesta de trabajo? 👽`
+
+    $('.whatsApp.no__link').onclick = () => {
+        const uri = `https://wa.me/51977156464?text=${encodeURI(message)}`
+        openNewTab(uri)
+    }
+}
+
+loadHiperLinks()
