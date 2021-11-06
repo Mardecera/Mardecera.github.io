4d2ad75ce985138364b082a56194715ed1c8c4c9
mardecera <mardecera.personal@gmail.com>
2021-11-06 15:44:12 -0500

    Adding popup message.

--- scripts/netlify/send_form.js
+++ scripts/netlify/send_form.js
@@ -1,16 +1,77 @@
+const MESSAGE = 'Your message was sent sucessfully! ✌'
 const $ = (query) => document.querySelector(query)
 
 const contactmeForm = $('#contactme')
 
-contactmeForm.addEventListener('submit', (event) => {
+contactmeForm.addEventListener('submit', async (event) => {
     event.preventDefault()
     let myForm = document.getElementById('contactme')
     let formData = new FormData(myForm)
-    fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: new URLSearchParams(formData).toString(),
-    })
-        .then(() => console.log('Form successfully submitted'))
-        .catch((error) => alert(error))
+    const buttonSubmit = myForm.querySelector('#submit')
+    try {
+        loadButton(buttonSubmit)
+        await fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: new URLSearchParams(formData).toString(),
+        })
+        sendButton(buttonSubmit)
+        notificacionPopup(MESSAGE, 'success')
+        contactmeForm.reset()
+    } catch (error) {
+        notificacionPopup(error, 'error')
+    }
 })
+
+function loadButton(button) {
+    cleanerElement(button)
+    const loaderDiv = document.createElement('div')
+    const div1 = document.createElement('div')
+    const div2 = document.createElement('div')
+    const div3 = document.createElement('div')
+    const div4 = document.createElement('div')
+    loaderDiv.classList.add('lds-ellipsis')
+    loaderDiv.appendChild(div1)
+    loaderDiv.appendChild(div2)
+    loaderDiv.appendChild(div3)
+    loaderDiv.appendChild(div4)
+    button.appendChild(loaderDiv)
+}
+
+function cleanerElement(element) {
+    while (element.lastChild) element.firstChild.remove()
+}
+
+function sendButton(button) {
+    cleanerElement(button)
+    const i = document.createElement('i')
+    i.classList.add('fas', 'fa-paper-plane')
+    button.textContent = 'Send'
+    button.appendChild(i)
+}
+
+function notificacionPopup(message, type) {
+    const i = document.createElement('i')
+    const element = document.querySelector('main')
+    const template = document.querySelector('#template__notification').content
+    const iconWrapper = template.querySelector('.notification__icon')
+
+    template.querySelector('.notification__message').textContent = message
+    cleanerElement(iconWrapper)
+    if (type === 'success') {
+        i.classList.add('fas', 'fa-check')
+    } else if (type === 'error') {
+        i.classList.add('fas', 'fa-ban')
+    }
+    iconWrapper.appendChild(i)
+    const clone = template.cloneNode(true)
+    element.appendChild(clone)
+    setTimeout(() => {
+        document
+            .querySelector('#notification__popup')
+            .classList.add('invisible')
+        setTimeout(() => {
+            document.querySelector('#notification__popup').remove()
+        }, 1500)
+    }, 4000)
+}
